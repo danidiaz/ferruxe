244df344b0cec9675a93edcee62d96114a35c4fe
Daniel Diaz <diaz.carrete@facebook.com>
2021-01-11 20:29:55 +0100

    Loops

---
+++ loopz/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    println!("Hello, world!");
+
+    // https://doc.rust-lang.org/book/ch03-05-control-flow.html
+    let mut _foo : () = loop {
+        break;
+    };
+
+    // https://doc.rust-lang.org/book/ch03-05-control-flow.html
+    let mut _foo2 : u32 = loop {
+        println!("Inside loop.");
+        break 2
+        // break 2; // also ok
+    };
+
+    // The if doesn't need a break to be expression-like
+    let _bar: u32  = if true { 3 } else { 4 };
+
+    let mut _uny : () = if true { () } else { () };
+    _uny = if true { println!("This is message"); }
+
+}
