7ded6e04304688cae8cd7dc66bf800b74f6f0fdb
Daniel Diaz <diaz.carrete@facebook.com>
2021-01-11 19:10:47 +0100

    Constant bindings and mutable references

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -14,4 +14,17 @@ fn main() {
     // https://www.reddit.com/r/rust/comments/6if67g/why_do_we_need_dereferencing_could_not_every_use/
     println!("This bar: {}",bar);
 
+    let mut xoo : &u32 = &foo;
+    println!("This xoo: {}",xoo);
+    xoo = &foo2;
+    println!("This xoo: {}",xoo);
+
+    // so... a constant binding can point to a *mutable* refernce?
+    let mut foo3 : u32 = 7;
+    let xee: &mut u32 = &mut foo3;
+    *xee = 4;
+    // It seems that dereferncing is required here
+    // "consider dereferencing here to assign to the mutable borrowed piece of memory"
+    // xee = 3; // Seems like de-referencig *is* necessary here...
+    println!("This xee: {}",xee);
 }
